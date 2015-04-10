72ea208581b694b3c7df55ee85ea31afdf6da670
Adonis K <aklp08@gmail.com>
2015-04-11 00:18:26 +0300

    ported 5 more color schemes

---
+++ guake-wryan.sh
@@ -0,0 +1,3 @@
+gconftool-2 -s -t string /apps/guake/style/font/color "#999999999393"
+gconftool-2 -s -t string /apps/guake/style/background/color "#101010101010"
+gconftool-2 -s -t string /apps/guake/style/font/palette "#333333333333:#8C8C46466565:#282873737373:#7C7C7C7C9999:#393955557373:#5E5E46468C8C:#313165658C8C:#89899C9CA1A1:#3D3D3D3D3D3D:#BFBF4D4D8080:#5353A6A6A6A6:#9E9E9E9ECBCB:#47477A7AB3B3:#7E7E6262B3B3:#60609696BFBF:#C0C0C0C0C0C0"
